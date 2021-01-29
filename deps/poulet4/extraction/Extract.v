3f3186b226b7a14b9a816a4c19656fe1d9fbcb3b
Ryan Doenges <me@ryandoeng.es>
2021-01-28 19:44:03 -0500

    extract Unpack module

--- deps/poulet4/extraction/Extract.v
+++ deps/poulet4/extraction/Extract.v
@@ -3,8 +3,9 @@ Require Import Coq.extraction.ExtrOcamlNativeString.
 Require Import Coq.extraction.ExtrOcamlNatInt.
 Require Import Petr4.ExtractOcamlBinNumsBigint.
 Require Syntax.
+Require Unpack.
 Require Extraction.
 
 Cd "extraction/lib/".
-Separate Extraction Syntax Typed.
+Separate Extraction Syntax Typed Unpack.
 Cd "../../".
