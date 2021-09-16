cc88b87676334691b2b0ff847438ab7a82ec7ee1
Ryan Doenges <me@ryandoeng.es>
2021-09-16 13:04:23 -0400

    fix extraction of Ops.v

--- deps/poulet4/extraction/Extract.v
+++ deps/poulet4/extraction/Extract.v
@@ -6,15 +6,15 @@ Require Import Coq.extraction.ExtrOcamlNatInt.
 Require Import Coq.Numbers.BinNums.
 
 Extract Inductive positive => "Bigint.t"
-  [ "(fun p -> Bigint.of_zarith_bigint (Z.succ (Z.mul (Z.of_int 2) (Bigint.to_zarith_bigint p))))"
-      "(fun p -> Bigint.of_zarith_bigint (Z.mul (Z.of_int 2) (Bigint.to_zarith_bigint p)))" "(Bigint.of_zarith_bigint Z.one)" ]
-  "(fun f2p1 f2p f1 p -> if Z.leq (Bigint.to_zarith_bigint p) Z.one then f1 () else let (q,r) = Z.div_rem (Bigint.to_zarith_bigint p) (Z.of_int 2) in if Z.equal r Z.zero then f2p (Bigint.of_zarith_bigint q) else f2p1 (Bigint.of_zarith_bigint q))".
+  [ "(fun p -> (Bigint.succ (Bigint.(of_int 2 * p))))"
+    "(fun p -> (Bigint.(of_int 2 *  p)))" "(Bigint.one)" ]
+  "(fun f2p1 f2p f1 p -> if Bigint.(p <= one) then f1 () else let (q, r) = Bigint.(p / of_int 2, rem p (Bigint.of_int 2)) in if Bigint.equal r Bigint.zero then f2p q else f2p1 q)".
 
-Extract Inductive Z => "Bigint.t" [ "(Bigint.of_zarith_bigint Z.zero)" "" "(fun z -> Bigint.of_zarith_bigint (Z.neg (Bigint.to_zarith_bigint z)))" ]
-  "(fun fO fp fn z -> let nz = (Bigint.to_zarith_bigint z) in let s = Z.sign nz in if s = 0 then fO () else if s > 0 then fp z else fn (Bigint.of_zarith_bigint (Z.neg nz)))".
+Extract Inductive Z => "Bigint.t" [ "Bigint.zero" "" "(fun z -> Bigint.neg z)" ]
+  "(fun fO fp fn z -> let s = Bigint.sign z in if s = Zero then fO () else if s = Pos then fp z else fn (Bigint.neg z))".
 
-Extract Inductive N => "Bigint.t" [ "(Bigint.of_zarith_bigint Z.zero)" "" ]
-  "(fun fO fp n -> if Z.sign (Bigint.to_zarith_bigint n) <= 0 then fO () else fp n)".
+Extract Inductive N => "Bigint.t" [ "(Bigint.zero)" "" ]
+  "(fun fO fp n -> (match Bigint.sign n with | Neg -> fO () | _ -> fp n))".
 
 Require Poulet4.SimplExpr.
 Require Poulet4.GenLoc.
